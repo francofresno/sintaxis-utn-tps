509d021563df8f13beddfc28e19900e45517c5a5
Franco <Franco@192.168.0.11>
2019-04-22 21:19:18 -0300

    agregados argumentos a main y char alfabeto

--- gramatica_suma_resta/src/gramatica_suma_resta.c
+++ gramatica_suma_resta/src/gramatica_suma_resta.c
@@ -11,7 +11,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+int main(int argc, char **argv) {
+
+	char alfabeto[14] = { '0','1','2','3','4','5','6','7','8','9','*','+','(',')' };
+
 	return EXIT_SUCCESS;
 }
